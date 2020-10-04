765f098d30e245aa5fbbb79334249576daeb17c8
victOr <dobr1nya@mail.ru>
2020-10-04 10:44:42 +0300

    feat: finish BasicJS task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,3 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = class DepthCalculator {};
+module.exports = class DepthCalculator {
+    calculateDepth(arr) {
+        1 + (arr instanceof Array ? arr.reduce(function(max, item) {
+            return Math.max(max, calculateDepth(item));
+        }, 0) : -1);
+    }
+};
