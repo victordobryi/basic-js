4eca58d2d339ea780d508478bc52742ff1e8af80
victOr <dobr1nya@mail.ru>
2020-10-13 12:55:12 +0300

    complete recursive task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,9 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-    calculateDepth(arr) {
-        1 + (arr instanceof Array ? arr.reduce(function(max, item) {
-            return Math.max(max, calculateDepth(item));
-        }, 0) : -1);
+
+    constructor() {
+        this.count = 1;
+        this.depthCount = 1;
     }
+
+    // calculateDepth(arr) {
+    //     if (Array.isArray(arr) == true) {
+    //         1 + arr.reduce(function(a, b) {
+    //             return Math.max.apply(a, this.calculateDepth(b));
+    //         });
+    //     } else return 0;
+
+    // };
+
+    calculateDepth(arr) {
+        if (Array.isArray(arr) === true) {
+            return 1 + arr.reduce((acc, value) => Math.max(this.calculateDepth(value), acc), 0);
+        } else {
+            return 0;
+        }
+
+    }
+
+
+
 };
